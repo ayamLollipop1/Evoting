c195596aca6ad6ba1f02cd0bd001510c035ade20
ayamLollipop1 <ayamlollipop@outlook.com>
2023-03-16 17:13:40 +0000

    new commit

--- evoting/admin/categories-admins/create-student.php
+++ evoting/admin/categories-admins/create-student.php
@@ -39,7 +39,7 @@ $department->execute();
     <div id="wrapper">
         <nav class="navbar header-top fixed-top navbar-expand-lg  navbar-dark bg-dark">
             <div class="container">
-                <a class="navbar-brand" href="#">LOGO</a>
+                <a class="navbar-brand" href="#">TTI VOTING SYSTEM</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
